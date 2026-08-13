9e09b97691dac61dbe9a4c904a11c998929ef152
Sandhiya Jasmine <sandhiyajasmine0604@gmail.com>
2026-08-13 10:55:48 +0530

    Connect backend to Aiven MySQL

--- config/db.js
+++ config/db.js
@@ -3,9 +3,13 @@ require("dotenv").config();
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 connection.connect((err) => {
